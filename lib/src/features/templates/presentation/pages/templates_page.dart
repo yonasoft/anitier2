3f39f18c87b66861def5f72515624262708ea42b
Kevin Chen <yonasoft7@gmail.com>
2025-02-18 17:53:49 -0500

    added more pages for basic navigation in main page

---
+++ lib/src/features/templates/presentation/pages/templates_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TemplatesPage extends StatelessWidget {
+  const TemplatesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Templates Page");
+  }
+}
