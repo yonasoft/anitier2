060e4821d1e9fb42e37cc7ea8936a7c65e3c68be
Kevin Chen <yonasoft7@gmail.com>
2025-02-17 21:01:10 -0500

    gave navigation item it's own class and refactored

---
+++ lib/src/features/skeleton/models/navigation_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NavigationItem {
+  final IconData icon;
+  final String label;
+  final String route;
+
+  NavigationItem({
+    required this.icon,
+    required this.label,
+    required this.route,
+  });
+}
--- lib/src/features/skeleton/presentation/pages/main_page.dart
+++ lib/src/features/skeleton/presentation/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:anitier2/src/features/skeleton/models/navigation_item.dart';
 import 'package:anitier2/src/features/skeleton/presentation/widgets/bot_navbar.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedNavIndex = 0;
-  final _navItems = ['activity', 'templates', 'saved', 'user'];
+  final _navigationItems = [
+    NavigationItem(icon: Icons.home, label: 'Home', route: 'home'),
+    NavigationItem(icon: Icons.article, label: 'Templates', route: 'templates'),
+    NavigationItem(icon: Icons.add, label: 'Create', route: 'create'),
+    NavigationItem(icon: Icons.favorite, label: 'Saved', route: 'saved'),
+    NavigationItem(icon: Icons.account_box, label: 'Account', route: 'account')
+  ];
+
   void _onTap(int index) => {
         setState(() {
           _selectedNavIndex = index;
@@ -25,6 +33,7 @@ class _HomePageState extends State<HomePage> {
       child: Scaffold(
         bottomNavigationBar: screenWidth < 600
             ? BotNavbar(
+                navItems: _navigationItems,
                 selectedIndex: _selectedNavIndex,
                 onTap: _onTap,
               )
