4f5452309e246244157114b28663fc3199555d30
Kevin Chen <chenkevin1119@gmail.com>
2025-02-17 21:01:10 -0500

    gave navigation item it's own class and refactored

--- lib/src/features/skeleton/presentation/widgets/bot_navbar.dart
+++ lib/src/features/skeleton/presentation/widgets/bot_navbar.dart
@@ -1,11 +1,14 @@
+import 'package:anitier2/src/features/skeleton/models/navigation_item.dart';
 import 'package:flutter/material.dart';
 
 class BotNavbar extends StatefulWidget {
+  final List<NavigationItem> navItems;
   final int selectedIndex;
   final Function(int) onTap;
 
   const BotNavbar({
     super.key,
+    required this.navItems,
     required this.selectedIndex,
     required this.onTap,
   });
@@ -17,17 +20,19 @@ class BotNavbar extends StatefulWidget {
 class _BotNavbarState extends State<BotNavbar> {
   @override
   Widget build(BuildContext context) {
+    final botNavItems = widget.navItems
+        .map((item) => item.route != 'create'
+            ? BottomNavigationBarItem(icon: Icon(item.icon), label: item.label)
+            : null)
+        .whereType<BottomNavigationBarItem>()
+        .toList();
+
     return BottomNavigationBar(
         currentIndex: widget.selectedIndex,
         onTap: (index) => {widget.onTap(index)},
         selectedItemColor: Color.fromRGBO(88, 133, 175, 1),
         unselectedItemColor: Colors.grey,
         type: BottomNavigationBarType.shifting,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.article), label: 'Templates'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Saved'),
-          BottomNavigationBarItem(icon: Icon(Icons.account_box), label: 'Account'),
-        ]);
+        items: [...botNavItems]);
   }
 }
