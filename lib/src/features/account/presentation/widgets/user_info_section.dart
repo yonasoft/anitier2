4e66ced120410b6f5326e54d95bd2187134eed50
Kevin Chen <chenkevin1119@gmail.com>
2025-02-21 13:50:10 -0500

    some minor UI changes and made user info section reusable for different users

--- lib/src/features/account/presentation/widgets/user_info_section.dart
+++ lib/src/features/account/presentation/widgets/user_info_section.dart
@@ -1,100 +1,34 @@
 import 'package:anitier2/src/core/constants.dart';
+import 'package:anitier2/src/features/account/presentation/widgets/user_avatar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-class UserInfoSection extends StatefulWidget {
-  const UserInfoSection({super.key});
-
-  @override
-  State<UserInfoSection> createState() => _UserInfoSectionState();
-}
-
-class _UserInfoSectionState extends State<UserInfoSection> {
-  User? _currentUser;
-  String? _avatar;
-  String? _displayName;
-  String? _email;
-
-  @override
-  void initState() {
-    super.initState();
-    _currentUser = FirebaseAuth.instance.currentUser;
-    _avatar = _currentUser?.photoURL;
-    _displayName = _currentUser?.displayName;
-    _email = _currentUser?.email;
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (mounted) {
-        setState(() {
-          _currentUser = user;
-          _avatar = _currentUser?.photoURL;
-          _displayName = _currentUser?.displayName;
-          _email = _currentUser?.email;
-        });
-      }
-    });
-  }
+class UserInfoSection extends StatelessWidget {
+  final User? _user;
+  const UserInfoSection(this._user, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    final String? avatar = _user?.photoURL;
+    final String? displayName = _user?.displayName;
+    final String? email = _user?.email;
+
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          CircleAvatar(
-            minRadius: 56,
-            child: ClipOval(
-              child: _avatar != null
-                  ? CachedNetworkImage(
-                      imageUrl: _avatar!,
-                      fit: BoxFit.cover,
-                      width: 112,
-                      height: 112,
-                      placeholder: (context, url) =>
-                          const Center(child: CircularProgressIndicator()),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.account_circle, size: 56),
-                    )
-                  : Icon(Icons.account_circle, size: 56),
-            ),
-          ),
+          UserProfileAvatar(avatar),
           SizedBox(height: 12),
-          Text(_displayName ?? "",
+          Text(displayName ?? "",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
           SizedBox(height: 8),
-          Text(_email ?? "", style: TextStyle(fontSize: 14)),
+          Text(email ?? "", style: TextStyle(fontSize: 14)),
           SizedBox(height: 12),
           ElevatedButton.icon(
               onPressed: () async {
-                bool shouldSignOut = await showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Confirm Sign Out'),
-                      content: Text('Are you sure you want to sign out?'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop(false);
-                          },
-                          child: Text('Cancel'),
-                        ),
-                        ElevatedButton(
-                          style: redButtonStyle,
-                          onPressed: () {
-                            Navigator.of(context).pop(true);
-                          },
-                          child: Text(
-                            'Sign Out',
-                            style: TextStyle(color: Colors.red.shade300),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
+                bool shouldSignOut = await showSignOutDialog(context);
 
                 if (shouldSignOut == true) {
                   FirebaseAuth.instance.signOut();
@@ -111,4 +45,35 @@ class _UserInfoSectionState extends State<UserInfoSection> {
       ),
     );
   }
+}
+
+Future<bool> showSignOutDialog(BuildContext context) async {
+  return (await showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Confirm Sign Out'),
+            content: Text('Are you sure you want to sign out?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: Text('Cancel'),
+              ),
+              ElevatedButton(
+                style: redButtonStyle,
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: Text(
+                  'Sign Out',
+                  style: TextStyle(color: Colors.red.shade300),
+                ),
+              ),
+            ],
+          );
+        },
+      )) ??
+      false;
 }
